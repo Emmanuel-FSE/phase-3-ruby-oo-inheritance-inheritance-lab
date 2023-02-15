4404c68b5f7d7f81653ac211ebb6fe1cedda3f97
Emmanuel-FSE <emmanuelmutisya254@gmail.com>
2023-02-15 22:51:09 +0300

    Lab completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative './user'
+class Student < User
+    def initialize()
+      @knowledge = []
+    end
 
+    def learn(new1)
+        @knowledge << new1
+    end
+
+    def knowledge
+        @knowledge
+    end
+    
+    
 end
+
+sophie = Student.new
+sophie.learn("Emmax")
+puts sophie.instance_variable_get(:@knowledge)
